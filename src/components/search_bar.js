870a9a8c675999b5f3a8e3767c435e931c326d07
Eduardo Francellino <eduardo@br.ibm.com>
2018-10-21 22:40:06 -0300

    changing version

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,7 +14,7 @@ class SearchBar extends Component {
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)}
         />
-        <label>&nbsp; v39</label>
+        <label>&nbsp; v40</label>
       </div>
     );
   }
